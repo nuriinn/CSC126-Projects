efa60bcbe6c9a70821e02e0893d82726c5134f2c
nurin <165421810+nuriinn@users.noreply.github.com>
2024-05-18 21:53:14 +0800

    Add files via upload

---
+++ [function] calculation fee.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Student's ID : ";
+    cin>>studentId;
+    cout<<"Student's name : ";
+    cin>>studentName;
+    cout<<"Type of study : ";
+    cin>>study;
+        if(study == 1){
+            cout<<"Field of study : ";
+            cin>>code;
+            if(code == 'A'){
+                fees = 60.00;
+                discount = 0.12;
+            }
+            else if (code == 'B'){
+                fees = 70.00;
+                discount = 0.00;
+            }
+        else if (study == 2){
+            cout<<"Field of study : ";
+            cin>>code;
+            if(code == 'A'){
+                fees = 65.00;
+                discount = 0.12;
+            }
+            else if (code == 'B'){
+                fees = 75.00;
+                discount = 0.00;
+            }
+        }
+        }
+        cout<<"Number of subject : ";
+        cin>>subject;
+
+    return 0;
+}
+double calcCredit ( int double , ){
+    for ( int credit = 0 ; credit )
+    cout<<"Enter credit hours : " <<
+}
